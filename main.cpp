6cfc8ed103a4d5d41851564e6c5b2e3cf694b1bb
Hao Zhou <857968442@qq.com>
2022-04-14 15:04:02 +0800

    删除递归控制弹射次数的代码

--- main.cpp
+++ main.cpp
@@ -22,8 +22,6 @@ using std::make_shared;
 // 俄罗斯轮盘赌算法参数
 const float RR_p = 0.8;
 
-bool is_RR = true;
-
 // 画面自定义参数
 int samping = 300; // samping per pixel
 int pixel_width = 1080; // 屏幕宽
@@ -42,27 +40,22 @@ T clamp(T x, T min, T max)
 	return x;
 }
 
-color ray_color(const ray& r, const vector<shared_ptr<hitable>>& things, int deep) {
+color ray_color(const ray& r, const vector<shared_ptr<hitable>>& things) {
 	hit_info rec;
 	color att;
 	ray sca;
-	if (!is_RR && deep <= 0) return color(0, 0, 0);
 	if (muti_hit(r, things, 0.00001, infinity, rec) && (*(rec.mat_ptr)).scatter(r, rec, att, sca)) {
 		if (rec.mat_ptr->is_light) {
 			//if(!is_RR)
 			return static_cast<light*>(rec.mat_ptr.get())->light_color;
 			//else return static_cast<light*>(rec.mat_ptr.get())->light_color *(1./RR_p);
 		}
-		if (is_RR) {
-			bool go_on = random_double() < RR_p;
-			if (go_on) {
-				return att * ray_color(sca, things, deep - 1) * (1. / RR_p);
-			}
-			else return vec3(0, 0, 0);
+		bool go_on = random_double() < RR_p;
+		if (go_on) {
+			return att * ray_color(sca, things) * (1. / RR_p);
 		}
-		else {
-			return att * ray_color(sca, things, deep - 1);
-		}
+		else return vec3(0, 0, 0);
+
 	}
 	return color(0, 0, 0);
 }
@@ -86,8 +79,7 @@ void write_color(std::ostream& out, color pixel_color, int samping) {
 	auto b = pixel_color.z();
 	float scale;
 	//根据样本数对颜色取平均值
-	if (is_RR) scale = 1.0 / samping;
-	else scale = 1.0 / samping;
+	scale = 1.0 / samping;
 	r = sqrt(r * scale);
 	g = sqrt(g * scale);
 	b = sqrt(b * scale);
@@ -107,14 +99,12 @@ int main(int argc, char *argv[]) {
 	srand((unsigned int)time(NULL));//随机种子
 	const double aspect_ratio = 16.0 / 9.0;
 	const int pixel_height = static_cast<int> (pixel_width / aspect_ratio);
-	const bool is_ssaa = false;   //是否开启SSAA（废弃）
     const bool is_random_select = true;
 	std::ofstream out_file;
 	out_file.open(file_name, std::ios::out | std::ios::trunc);
 
 	cout << "samping: " << samping << "  resolution: " <<pixel_height<<"*"<< pixel_width << "  file_name: " << file_name << std::endl;
 
-	const int ray_deep = 8;
 	const int pixel_samping = samping;
     shared_ptr<lambertian> lbt_ptr(new lambertian({1-117./255.,1-49./255.,1- 142./255.}));
 	shared_ptr<lambertian> lbt_red(new lambertian({ 0.9,0,0 }));
@@ -160,7 +150,6 @@ int main(int argc, char *argv[]) {
 	// render
 	double u, v;
 	ray r;
-	//cout << "P3\n" << pixel_width << " " << pixel_height << "\n255\n";
 	out_file << "P3\n" << pixel_width << " " << pixel_height << "\n255\n";
 
 	//Tip：这里因为用用了整数，导致unit_sssaa值为0
@@ -173,40 +162,30 @@ int main(int argc, char *argv[]) {
 	int jin_du_tiao = 0;
 	for (int j = pixel_height - 1; j >= 0; --j) {
 		for (int i = 0; i < pixel_width; ++i) {
+			// 单个像素内随机采样
+			//TODO: 面试官问用等距代替随机会有什么不同
 			u = double(i) / (pixel_width - 1);
 			v = double(j) / (pixel_height - 1);
 			color final_col;
 
-			if (is_ssaa) {
-				// 超采样
-				color sum_col = color();
-
-				for (int p = 0; p < 4; p++) {
-					r = cam.get_ray(u + SSAA[p][0], v + SSAA[p][1]);
-					auto temp = ray_color(r, world,ray_deep);
-					sum_col += temp;
-				}
-				final_col = sum_col / 4;
-			}
-			else if(is_random_select) {
+			if(is_random_select) {
 				color sum_col = color();
                 for (int p = 0; p < pixel_samping; p++) {
                     double ut = u + random_double(0,2)*unit_ssaa[0];
                     double uv = v + random_double(0, 2) * unit_ssaa[1];
                     r = cam.get_ray(ut, uv);
 					//r = cam.get_ray(u, v);
-                    auto temp = ray_color(r,world, ray_deep);
+                    auto temp = ray_color(r,world);
                     sum_col+=temp;
                 }
-                //final_col = sum_col / pixel_samping;
 				final_col = sum_col;
 			} else {
-                final_col = ray_color(cam.get_ray(u, v), world, ray_deep);
+                final_col = ray_color(cam.get_ray(u, v), world);
             }
 
-			//write_color(cout, final_col, pixel_samping);
 			write_color(out_file, final_col, pixel_samping);
 		}
+		// 命令行进度条 windows only
 		float percent = ((float)(pixel_height - j) / (float)pixel_height) * 100.;
 		if (percent > jin_du_tiao * 5) {
 			if(jin_du_tiao != 0) cout << " -> ";
